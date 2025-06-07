2dbd88b806581c952ba416cb2a1b7dcc199b54c6
viratxd <38873038+viratxd@users.noreply.github.com>
2025-06-07 12:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1019,4 +1019,4 @@ def index(request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=5001, timeout_keep_alive=120)
+    uvicorn.run(app, host="0.0.0.0", port=7860, timeout_keep_alive=120)
